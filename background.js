b7d728be3e368829d2e6b66e33ce5be736e88ad6
theSpawacz <56389913+maciejstepie@users.noreply.github.com>
2022-12-14 16:00:03 +0100

    update i guess

--- background.js
+++ background.js
@@ -8,23 +8,26 @@ function Get(atr) {
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    SetChrome({ name: "maciek" });
+    SetChrome({ name: "testerfester" });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // console.log(tabId, changeInfo, tab);
 
-    if (changeInfo.status === "complete")
+    if (changeInfo.status === "complete") {
+        //css
         chrome.scripting
             .insertCSS({ target: { tabId }, files: ["./src/css/inject.css"] })
             .then(() => {
-                console.log("injected css!");
+                // console.log("injected css!");
             })
             .catch((err) => console.log(err));
-    chrome.scripting
-        .executeScript({ target: { tabId }, files: ["./foreground.js"] })
-        .then(() => {
-            console.log("injected!");
-        })
-        .catch((err) => console.log(err));
+        //script
+        chrome.scripting
+            .executeScript({ target: { tabId }, files: ["./foreground.js"] })
+            .then(() => {
+                //  console.log("injected!");
+            })
+            .catch((err) => console.log(err));
+    }
 });
--- foreground.js
+++ foreground.js
@@ -1,9 +1,37 @@
+//console.log("foreground test");
 let inject_body = document.querySelector("body");
-//body.style.backgroundColor = "green";
+let availableButotns = document.querySelector("#site_content > div.pagination");
+if (availableButotns) availableButotns = availableButotns.children;
+let prev = null;
+let next = null;
 
-const div = document.createElement("div");
-div.className = "pepe";
-div.innerHTML = "Następny";
-console.log(document.querySelector("#site_content > div.pagination"));
+for (const el of availableButotns) {
+    if (el.title === "Wstecz") {
+        prev = el.href;
+        // console.log(prev);
+    }
+    if (el.title === "Dalej") {
+        next = el.href;
+    }
+}
 
-inject_body.appendChild(div);
+if (next) {
+    //Next divw
+    const divNext = document.createElement("div");
+    divNext.className = "pepe next";
+    divNext.innerHTML = "<span>Następny</span><span>(" + next.split("/")[next.split("/").length - 1] + ")</span>";
+    divNext.addEventListener("click", () => redirectClick(next));
+    inject_body.appendChild(divNext);
+}
+
+//previous div
+if (prev) {
+    const divPrevious = document.createElement("div");
+    divPrevious.className = "pepe previous";
+    divPrevious.innerHTML = "<span>Poprzedni</span><span>(" + prev.split("/")[prev.split("/").length - 1] + ")</span>";
+    divPrevious.addEventListener("click", () => redirectClick(prev));
+    inject_body.appendChild(divPrevious);
+}
+function redirectClick(url) {
+    window.location.href = url;
+}
